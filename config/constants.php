910c645ddd1fcbe61f6610973df143eb1a5213e4
Yousef Ahmed AbdulRahman <125695657+yousefahmed2004@users.noreply.github.com>
2025-03-30 07:48:02 +0200

    Add files via upload

---
+++ config/constants.php
@@ -0,0 +1,17 @@
+<?php
+if (!defined('ROOT_URL')) {
+    define('ROOT_URL', 'http://localhost/fmp/');
+}
+if (!defined('DB_HOST')) {
+    define('DB_HOST', 'localhost');
+}
+if (!defined('DB_USER')) {
+    define('DB_USER', 'root');
+}
+if (!defined('DB_PASS')) {
+    define('DB_PASS', '');
+}
+if (!defined('DB_NAME')) {
+    define('DB_NAME', 'children_db');
+}
+?>
